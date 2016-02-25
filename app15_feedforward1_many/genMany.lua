5aa7c898019247da66a3c7a76d25d07dfedea839
Ameen Eetemadi <ameenetemady@github.com>
2016-02-25 13:28:08 -0800

    random parametrization of feedforward1.xml

---
+++ app15_feedforward1_many/genMany.lua
@@ -0,0 +1,15 @@
+local lfs = require 'lfs'
+require('./GnwNet')
+
+local currDir = lfs.currentdir()
+local settings = { baseDir = currDir, 
+                   templateDir = currDir .. "/template",
+                   xmlFilename = "feedforward1.xml"}
+
+local nNets = 10
+for i=1, nNets do
+  local currNet = GnwNet.new(settings, i)
+  currNet:init()
+  currNet:randomize()
+  currNet:run()
+end
