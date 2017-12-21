72a85e1a2b7e0a269671c7932cd9e468dbeafe3d
Ameen Eetemadi <ameenetemady@github.com>
2017-12-21 12:14:07 -0800

    cascase20 changes hanging around...

--- app20_cascade5/runOne.sh
+++ app20_cascade5/runOne.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-th ../common/genMany_netParam_sKO.lua cascade5.xml 100 > out.txt 2> err.txt
+#th ../common/genMany_netParam_sKO.lua cascade5.xml 5 0.05 > out.txt 2> err.txt
+th ../common/genMany_netParam_sKO.lua cascade5.xml 20 0.05 > out.txt 2> err.txt
+#th ../common/genMany_netParam_sKO.lua cascade5.xml 20 0.05 
